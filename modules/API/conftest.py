69d1fcaabffc6bc2cc385deba497386b1e4c6656
Rustam <safariggg@gmail.com>
2025-01-03 01:53:51 +0300

    fix

--- modules/API/conftest.py
+++ modules/API/conftest.py
@@ -1,7 +1,6 @@
-<<<<<<< HEAD
 import pytest
 from config import Authorization, API_KEY, USERNAME, PASSWORD
-from modules.API.meths import PetstoreClient
+from pw_pets.modules.API.meths import PetstoreClient
 
 @pytest.fixture(scope="function")
 def petstore_client():
@@ -9,4 +8,5 @@ def petstore_client():
     Фикстура для клиента Petstore.
     """
     client = PetstoreClient(api_key=API_KEY)
+    yield client
     # Здесь можно добавить teardown, если необходимо 
