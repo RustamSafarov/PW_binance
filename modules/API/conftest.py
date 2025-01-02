6690f4a6bc6e061597592f8c1b5295ebc381a1d2
Rustam Safarov <49519296+RustamSafarov@users.noreply.github.com>
2025-01-03 02:01:56 +0300

    Update conftest.py

--- modules/API/conftest.py
+++ modules/API/conftest.py
@@ -1,16 +1,3 @@
-<<<<<<< HEAD
-import pytest
-from config import Authorization, API_KEY, USERNAME, PASSWORD
-from modules.API.meths import PetstoreClient
-
-@pytest.fixture(scope="function")
-def petstore_client():
-    """
-    Фикстура для клиента Petstore.
-    """
-    client = PetstoreClient(api_key=API_KEY)
-    yield client
-=======
 import pytest
 from pw_pets.config import Authorization, API_KEY, USERNAME, PASSWORD
 from PW_pets.modules.API.meths import PetstoreClient
@@ -22,5 +9,4 @@ def petstore_client():
     """
     client = PetstoreClient(api_key=API_KEY)
     yield client
->>>>>>> 722a1c2e040ca93c033ace5debc6177c3c44ec5a
     # Здесь можно добавить teardown, если необходимо 
