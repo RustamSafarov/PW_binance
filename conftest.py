c1e16f45f87ec731d97af627c60aee2f8b6cf68e
Rustam Safarov <49519296+RustamSafarov@users.noreply.github.com>
2025-01-03 00:56:52 +0300

    Add files via upload

---
+++ conftest.py
@@ -0,0 +1,22 @@
+import pytest
+from pw_pets.config import Authorization, API_KEY, USERNAME, PASSWORD
+
+@pytest.fixture(scope="session")
+def auth_client():
+    """
+    Фикстура для авторизации и предоставления клиента API.
+    Выполняет вход в систему перед запуском тестов и выход после завершения.
+    """
+    auth = Authorization(api_key=API_KEY)
+    login_response = auth.login_user(USERNAME, PASSWORD)
+    assert login_response.get("code") == 200, "Авторизация не удалась"
+    yield auth
+    logout_status = auth.logout_user()
+    assert logout_status == 200, "Выход из системы не удался"
+
+@pytest.fixture(scope="session")
+def petstore_client(auth_client):
+    """
+    Фикстура для клиента Petstore, использующая авторизованный клиент.
+    """
+    return auth_client  # В данном случае Authorization тоже выполняет роль клиента API
